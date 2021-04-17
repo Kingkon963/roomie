2fabf590e3ff6ddf196adc3eb15d14ce9e470221
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2021-04-17 16:24:17 +0600

    pagination fixed

--- components/categoryTiles.tsx
+++ components/categoryTiles.tsx
@@ -3,8 +3,8 @@ import styles from '../styles/categoryTiles.module.scss';
 export default function CategoryTiles({ categories } : { categories: {name : string}[] }) {
   const tiles = categories.map((cat: {id: number, name: string}) => (
     <div className="column is-6 p-0" key={cat.id}>
-      <div className={`block ${styles.tile}`}>
-        <span className="icon-text is-flex is-flex-direction-column">
+      <div className={`is-flex is-justify-content-center  is-align-items-center ${styles.tile}`}>
+        <span className="icon-text is-flex is-flex-direction-column is-align-items-center">
           <span className="icon is-align-self-center">
             <i className={`fas fa-home ${styles.icon}`}>
               {}
@@ -15,6 +15,25 @@ export default function CategoryTiles({ categories } : { categories: {name : str
       </div>
     </div>
   ));
+
+  const blankTile = (
+    <div className="column is-6 p-0">
+      <div className={`is-flex is-justify-content-center  is-align-items-center ${styles.tileHeight}`}>
+        <span className="icon-text is-flex is-flex-direction-column is-align-items-center">
+          <span className="icon is-align-self-center">
+            <i className={`s ${styles.icon}`}>
+              {}
+            </i>
+          </span>
+          <span className="is-unselectable mt-3">{}</span>
+        </span>
+      </div>
+    </div>
+  );
+
+  while (tiles.length < 6) {
+    tiles.push(blankTile);
+  }
 
   return (
     <div className={`columns is-flex-wrap-wrap mt-2 px-0 has-text-white is-mobile ${styles.tileGrid}`}>
